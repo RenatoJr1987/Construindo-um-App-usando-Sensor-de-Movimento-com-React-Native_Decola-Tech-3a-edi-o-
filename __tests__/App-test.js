e16c5d40287a5b04fde06e8622758f941fc9ca55
Renato Domingues <95724654+RenatoJr1987@users.noreply.github.com>
2022-06-24 11:19:59 -0300

    sensor

---
+++ __tests__/App-test.js
@@ -0,0 +1,13 @@
+ @format
+
+
+import 'react-native';
+import React from 'react';
+import App from '../App';
+
+// test renderer must be required after react-native.
+import renderer from 'react-test-renderer';
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
